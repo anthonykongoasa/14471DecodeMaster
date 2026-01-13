82d457dd2e7ff11255d8898cb43f7e76dc2435f2
anthonykongoasa <anthonykongoasa32@gmail.com>
2026-01-13 13:43:13 -0500

    Update drive parameters for MecanumDrive

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/Drive/MecanumDrive.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/Drive/MecanumDrive.java
@@ -66,7 +66,7 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
         // drive model parameters
-        public double inPerTick = 39.5/21000;
+        public double inPerTick = 0.00188095238;
         public double lateralInPerTick = 0.00199421545;
         public double trackWidthTicks = 4791.752477044702;
 
@@ -76,9 +76,9 @@ public final class MecanumDrive {
         public double kA = 0.000045;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 55;
+        public double minProfileAccel = -35;
+        public double maxProfileAccel = 40;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
