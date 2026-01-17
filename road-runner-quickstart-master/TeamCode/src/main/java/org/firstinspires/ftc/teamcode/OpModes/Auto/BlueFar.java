426a694a0071f9931968ccb0a3f407d77379fdfa
anthonykongoasa <anthonykongoasa32@gmail.com>
2026-01-17 12:44:41 -0500

    Create BlueFar.java

---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/BlueFar.java
@@ -0,0 +1,144 @@
+
+package org.firstinspires.ftc.teamcode.OpModes.Auto;
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.Robot.BaseRobot14471;
+
+@Autonomous(name = "BlueFar")
+public class BlueFar extends LinearOpMode {
+
+    Pose2d startPose = new Pose2d(62, -16, Math.toRadians(0));
+    //AUTO robot constructor
+    BaseRobot14471 robot = new BaseRobot14471(true, startPose);
+    ElapsedTime runtime = new ElapsedTime();
+
+    double FARVEL = 1300;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //efficencize code !
+        robot.init(hardwareMap);
+        robot.testPIDF(120, 0.8, 0, 12);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        robot.leftShooter.setVelocity(-FARVEL);
+        robot.rightShooter.setVelocity(FARVEL);
+
+       Vector2d shootPos = new Vector2d(56, -16);
+        Pose2d shootPose2d  = new Pose2d(56, -16, Math.toRadians(20));
+
+        
+        Actions.runBlocking(
+                robot.drive.actionBuilder(startPose)
+
+                        // SHOOT #1
+                        .strafeToLinearHeading(shootPos, Math.toRadians(20))
+                        .build()
+        );
+        delay(0.2); // wait for shooter to get warmed up
+        robot.shoot();
+        delay(0.85);
+        robot.stopShootingAuto();
+
+        //intake 1 (motif)
+        Actions.runBlocking(
+                robot.drive.actionBuilder(shootPose2d)
+                        .strafeToLinearHeading(new Vector2d(35,-30),Math.toRadians(-90))
+                        .strafeToLinearHeading(new Vector2d(35,-55),Math.toRadians(-90))
+
+                        .build()
+        );
+        // shoot 3
+        robot.intake.setPower(-0.1);
+        Actions.runBlocking(
+                robot.drive.actionBuilder(new Pose2d(35, -55, Math.toRadians(-90)))
+                        .strafeToLinearHeading(shootPos, Math.toRadians(20))
+                        .build()
+        );
+
+        robot.shoot();
+        delay(0.85);
+        robot.stopShootingAuto();
+        //------------------------------intake POSITION #2---------------- (HP ZONE)
+        Actions.runBlocking(
+                robot.drive.actionBuilder(shootPose2d)
+                        
+                        .strafeToLinearHeading(new Vector2d(56,-50),Math.toRadians(-90)) //get in position
+          
+                        .lineToY(-60)
+                        .lineToY(-53)
+                        .lineToY(-60)
+
+                        .build()
+        );
+        robot.intake.setPower(-0.1);
+        Actions.runBlocking(
+                robot.drive.actionBuilder(new Pose2d(56, -60, Math.toRadians(-90)))
+          
+                        .strafeToLinearHeading(new Vector2d(50, -16), Math.toRadians(20))
+                        .strafeToLinearHeading(shootPos, Math.toRadians(20))
+                        .build()
+        );
+
+        // ------------shoot #2-------------
+
+
+        robot.shoot();
+        delay(0.85);
+        robot.stopShootingAuto();
+        
+        //intake 3 (HP ZONE X2)
+
+        Actions.runBlocking(
+                robot.drive.actionBuilder(shootPose2d)
+                        .strafeToLinearHeading(new Vector2d(56,-50),Math.toRadians(-90))
+                       // .strafeToLinearHeading(new Vector2d(56,-60),Math.toRadians(-92))
+                        .lineToY(-60)
+                        .lineToY(-53)
+                        .lineToY(-60)
+
+                        .build()
+        );
+        //shoot #4
+        robot.intake.setPower(-0.1);
+        Actions.runBlocking(
+                robot.drive.actionBuilder(new Pose2d(56, -50, Math.toRadians(-90)))
+                        .strafeToLinearHeading(new Vector2d(50, -16), Math.toRadians(20))
+                        .strafeToLinearHeading(shootPos, Math.toRadians(20))
+                        .build()
+        );
+        robot.shoot();
+        delay(0.85);
+        robot.stopShooting(); // turn off shooter motors
+
+
+        Actions.runBlocking( // off line, setup for tele
+                robot.drive.actionBuilder(shootPose2d)
+
+                         .strafeToLinearHeading(new Vector2d(25,-16),Math.toRadians(-90))
+
+                        .build()
+        );
+
+    }
+
+
+    // -------- Delay helper ----------
+    private void delay(double t) {
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < t)) {
+            // Optionally update telemetry
+        }
+    }
+}
